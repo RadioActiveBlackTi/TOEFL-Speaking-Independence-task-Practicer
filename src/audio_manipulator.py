8a41f8baa219169f0ef945667c247195721b568b
RadioActiveBlackTi <dorae0520@naver.com>
2024-01-22 23:24:33 +0900

    utility implemented

---
+++ src/audio_manipulator.py
@@ -0,0 +1,85 @@
+from gtts import gTTS
+from playsound import playsound
+
+import os,sys
+import time
+
+import queue, threading
+import sounddevice as sd
+import soundfile as sf
+
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+def topic_tts(topic):
+    tts = gTTS(
+        text=topic,
+        lang='en', slow=False
+    )
+    tts.save(resource_path('resources/topic.mp3'))
+    t = time.time()
+    playsound(resource_path('resources/topic.mp3'))
+    t2 = time.time()
+    print(t2-t)
+
+def prepare_tts():
+    t = time.time()
+    playsound(resource_path('resources/prepare.mp3'))
+    t2 = time.time()
+    print(t2 - t)
+
+def speaknow_tts():
+    t = time.time()
+    playsound(resource_path('resources/speaknow.mp3'))
+    t2 = time.time()
+    print(t2 - t)
+
+
+q = queue.Queue()
+recorder = False
+recording = False
+
+
+def complicated_record():
+    with sf.SoundFile(resource_path('resources/record.wav'), mode='w', samplerate=16000, subtype='PCM_16', channels=1) as file:
+        with sd.InputStream(samplerate=16000, dtype='int16', channels=1, callback=complicated_save):
+            while recording:
+                file.write(q.get())
+
+
+def complicated_save(indata, frames, time, status):
+    q.put(indata.copy())
+
+
+def start():
+    global recorder
+    global recording
+    recording = True
+    recorder = threading.Thread(target=complicated_record)
+    print('start recording')
+    recorder.start()
+
+
+def stop():
+    global recorder
+    global recording
+    recording = False
+    recorder.join()
+    print('stop recording')
+
+if __name__=="__main__":
+start()
+time.sleep(10)
+stop()
+
+topic_tts("Would you prefer to work in a team or work alone on a project? Include details and explanation.")
+prepare_tts()
+speaknow_tts()
--- src/gpt_parser.py
+++ src/gpt_parser.py
@@ -1,2 +1,65 @@
-import openai
+from openai import OpenAI
 import re
+
+import sys, os
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+# api key getter
+with open(resource_path("gpt-api-key.txt"), "r") as file:
+    api_key = file.read()
+
+client = OpenAI(
+    api_key=api_key
+)
+
+def get_topic():
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user",
+                  "content": """Can you provide ONE TOPIC example of TOEFL speaking independence task 
+                  with agreement/disagreement or preference style?
+                  
+                  TOPIC SUGGESTION means specific topic like 'Would you prefer to take a challenging course or an easy course at university?".
+                  DESCRIPTION means some words like "Include details and explanation." or "State whether you agree or disagree with the following statement. Then explain your reasons using specific details in your explanation."
+                  
+                  Please answer ONLY with this format. DO NOT USE WORDS LIKE "Topic: ", "Description: ".:
+                  
+                  Suggestion: <Paragraph that combined TOPIC SUGGESTION and DESCRIPTION>"""}
+        ]
+    )
+
+    chat_response = completion.choices[0].message.content
+    p = re.compile('Suggestion:[ \t\n\r\f\v]')
+    result = p.sub('', chat_response)
+    print(result)
+
+    return result
+
+def get_sample_response(topic: str):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user",
+                   "content": f"""Give me the well-done possible 45-second-response script composed with 2 reasons of this topic:
+                   
+                   [{topic}]
+
+                    Please answer ONLY with this format. Please divide paragraphs so that we can see it visually easily:
+
+                    Possible Response: <Your possible response for given topic>"""}
+                  ]
+    )
+
+    chat_response = completion.choices[0].message.content
+    print(chat_response)
+
+if __name__=="__main__":
+    topic = get_topic()
+    get_sample_response(topic)
